e1ae53349f255f1d60991d3801414c2b8cfb9ab3
factor380 <wer380@gmail.com>
2017-12-10 21:35:59 +0200

    i do privare

--- be_MiniPOrject/be_MiniPOrject/Contract.cs
+++ be_MiniPOrject/be_MiniPOrject/Contract.cs
@@ -6,19 +6,30 @@ using System.Threading.Tasks;
 
 namespace be_MiniPOrject
 {
-    //bd ha gever
+    
     class Contract
     {
         static int ContractNum;
-        int NannyId;
-        int ChildId;
-        bool met;
-        bool ActiveContract;
-        float PayHours;
-        int PayMonth;
-        bool HorM;//the Pay per month or hour 
-        DateTime StartDate;
-        DateTime EndDate;
+        private int nannyId;
+        private int childId;
+        private bool met;
+        private bool activeContract;
+        private float payHours;
+        private int payMonth;
+        private bool horM;//the Pay per month or hour 
+        private DateTime startDate;
+        private DateTime endDate;
+
+        public static int ContractNum1 { get => ContractNum; set => ContractNum = value; }
+        public int NannyId { get => nannyId; set => nannyId = value; }
+        public int ChildId { get => childId; set => childId = value; }
+        public bool Met { get => met; set => met = value; }
+        public bool ActiveContract { get => activeContract; set => activeContract = value; }
+        public float PayHours { get => payHours; set => payHours = value; }
+        public int PayMonth { get => payMonth; set => payMonth = value; }
+        public bool HorM { get => horM; set => horM = value; }
+        public DateTime StartDate { get => startDate; set => startDate = value; }
+        public DateTime EndDate { get => endDate; set => endDate = value; }
 
         public override string ToString()
         {
