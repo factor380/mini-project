bdced963817e59b5c5a35a00fc523a4c8a1e2fa5
factor380 <wer380@gmail.com>
2017-12-10 22:43:54 +0200

    idal

---
+++ be_MiniPOrject/dal/idal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using be_MiniPOrject;
+
+namespace dal
+{
+    public interface idal
+    {
+        void AddNanny();
+        void DelNanny();
+        void updatingNanny();
+
+        void AddMother();
+        void DelMother();
+        void updatingMother();
+        void AddChild();
+        void DelChild();
+        void updatingChild();
+        void AddContract();
+        void DelContract();
+        void updatingContract();
+        List<Nanny> AcceptanceNanny();
+        List<Mother> AcceptanceMother();
+        List<Child> AcceptanceChild();
+        List<Contract> AcceptanceContract();
+
+
+
+
+    }
+}
