2acfefc86e5cdd20781597b57800c08bbac47ec1
bdelkayam <bdelkayam@gmail.com>
2018-01-01 22:59:11 +0200

    הוספתי חלון הוספה לחוזה ולילד

--- be_MiniPOrject/UL/ContractWindow.xaml.xaml.cs
+++ be_MiniPOrject/UL/ContractWindow.xaml.xaml.cs
@@ -23,5 +23,13 @@ namespace UL
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource contractViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("contractViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // contractViewSource.Source = [generic data source]
+        }
     }
 }
