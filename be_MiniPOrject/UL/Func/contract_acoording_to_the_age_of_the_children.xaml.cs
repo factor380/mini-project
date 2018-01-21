5ffd481f22448e487435114ded06b42c9b155774
factor380 <wer380@gmail.com>
2018-01-21 21:43:28 +0200

    תיקנתי מקווה

---
+++ be_MiniPOrject/UL/Func/contract_acoording_to_the_age_of_the_children.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UL
+{
+    /// <summary>
+    /// Interaction logic for contract_acoording_to_the_age_of_the_children.xaml
+    /// </summary>
+    public partial class contract_acoording_to_the_age_of_the_children : Window
+    {
+        public contract_acoording_to_the_age_of_the_children()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ be_MiniPOrject/UL/Func/contract_under_certian_conditions.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UL
+{
+    /// <summary>
+    /// Interaction logic for contract_under_certian_conditions.xaml
+    /// </summary>
+    public partial class contract_under_certian_conditions : Window
+    {
+        public contract_under_certian_conditions()
+        {
+            InitializeComponent();
+        }
+    }
+}
